1d00c17dd8cb48114979169b5f005a73e4191339
ashill99 <admshng@gmail.com>
2021-01-05 15:00:27 -0500

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,30 +11,44 @@ Post.destroy_all
 Trip.destroy_all
 User.destroy_all
 
-puts "🍝 Seeding Users..."
-2.times do
-  user = User.create!(
-    name: Faker::Food.dish,
-    username: Faker::Artist.name
-  )
+
+user1 = User.create!(name: 'Adam', username: 'adam1')
+user2 = User.create!(name: 'Vivek', username: 'vik')
 
 
-  2.times do
-    trip = Trip.create!(
-      location: Faker::Food.ingredient,
-      date: Faker::Food.measurement,
-      user: user
-    )
- 
-  2.times do
-    Post.create!(
-      caption: Faker::Food.ingredient,
-      img_url: Faker::Avatar.image,
-      like: 0,
-      trip: trip
-    )
-    end
-end
-end
+# puts "🍝 Seeding Users..."
+# 2.times do
+#   user = User.create!(
+#     name: Faker::Food.dish,
+#     username: Faker::Artist.name
+#   )
+
+trip1 = Trip.create!(location: "London", date: "11/12", user: user1)
+trip2 = Trip.create!(location: "New York", date: "9/15", user: user1)
+
+#   2.times do
+#     trip = Trip.create!(
+#       location: Faker::Food.ingredient,
+#       date: Faker::Food.measurement,
+#       user: user
+#     )
+
+post1 = Post.create!(caption: 'Great Trip', img_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/City_of_London%2C_seen_from_Tower_Bridge.jpg/250px-City_of_London%2C_seen_from_Tower_Bridge.jpg', like: 0, trip: trip1)
+post2 = Post.create!(caption: "So many rats", img_url: "https://www.google.com/search?q=new+york+pictures&rlz=1C5CHFA_enUS922US922&sxsrf=ALeKk02S2fiNpcr7O_qnKoqRFRX9Lc1TIA:1609865049250&tbm=isch&source=iu&ictx=1&fir=CJ2vZyOUZEThKM%252CXUerpqP9pwXzGM%252C_&vet=1&usg=AI4_-kQtH_MNfMpMyZvpwH75xfLgPckhew&sa=X&ved=2ahUKEwil6JXTnoXuAhXHJt8KHZJGDoYQ9QF6BAgIEAE#imgrc=CJ2vZyOUZEThKM", like: 0, trip: trip2)
+
+post3 = Post.create!(caption: 'Big Smoke', img_url: 'https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg', like: 0, trip: trip1)
+post4 = Post.create!(caption: "Big Apple", img_url: "https://untappedcities.com/wp-content/uploads/2017/11/Staten-Island-Ferry-NYC_3.jpg", like: 0, trip: trip2)
+
+
+#   2.times do
+#     Post.create!(
+#       caption: Faker::Food.ingredient,
+#       img_url: Faker::Avatar.image,
+#       like: 0,
+#       trip: trip
+#     )
+#     end
+# end
+# end
 puts "🎉 Done!"
 
