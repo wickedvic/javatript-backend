d27b771f9b0b610fbfa9f6b94111fdee9a604878
wickedvic <viksipart@gmail.com>
2021-01-04 20:04:21 -0500

    partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-namespace :api do
-  namespace :v1 do
-    resources :trips, only: [:show]
+# namespace :api do
+#   namespace :v1 do
+#     resources :trips, only: [:show]
+  resources :trips
 
 
-
-    end
-  end
+#     end
+#   end
 end
