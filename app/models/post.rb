ec83ff0e05f81bb30875d28e962c8dc77c1cf1f7
ashill99 <admshng@gmail.com>
2021-01-08 10:30:35 -0500

    added active storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :trip
   has_many :comments, dependent: :destroy 
   delegate :user, to: :trip
+  # has_one_attached :image, :dependent => :destroy
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,9 @@
 class PostSerializer < ActiveModel::Serializer
     attributes :id, :caption, :trip_id, :img_url, :like, :comments
+
+    # def avatar
+    #   rails_blob_path(object.image, only_path: true) if object.image.attached?
+    # end
+
   end
   
